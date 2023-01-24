2d6be987c42cbaf666e377d6764aed95b72d0be8
David-innov8 <115226033+David-innov8@users.noreply.github.com>
2023-01-24 11:22:34 +0100

    Create Dropdown.jsx

---
+++ ecom/src/Components/Dropdown.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+ const Dropdown = () => {
+  return (
+    <div className=''>
+        <h1>Custom drop down</h1>
+        <div className='DropdownCotainer'>
+            <div className='DropDownHeader'>Mangoes</div>
+       
+            <div className="DropdownListContainer">
+
+
+            
+        <ul>
+            <li>Mangoes</li>
+            <li>beans</li>
+            <li>Rice</li>
+        </ul>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+
+export default Dropdown;
