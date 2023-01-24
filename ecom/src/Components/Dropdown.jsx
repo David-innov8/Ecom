182633faeca503e639b0ca13c66de95d94dcdc23
David-innov8 <115226033+David-innov8@users.noreply.github.com>
2023-01-24 11:50:06 +0100

    Update Dropdown.jsx

--- ecom/src/Components/Dropdown.jsx
+++ ecom/src/Components/Dropdown.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import {React, usestate} from 'react'
 
  const Dropdown = () => {
+
+    const [isOpen, setIsOpen] = usestate(false)
+
+    const toggle = ()=>{
+        setIsOpen(!isOpen)
+    }
+
   return (
     <div className=''>
         <h1>Custom drop down</h1>
-        <div className='DropdownCotainer'>
-            <div className='DropDownHeader'>Mangoes</div>
+        <div className= ' m-auto w-40 DropdownCotainer'>
+            <div onClick={toggle} className='DropDownHeader  mb-3 py-2 px-8 font-medium text-lg'>Mangoes</div>
        
             <div className="DropdownListContainer">
 
 
             
-        <ul>
+        <ul className='DropdownList  p-0 m-0 pl-4 bg-white border-2 text-lg font-medium first:pt-3 ' >
             <li>Mangoes</li>
             <li>beans</li>
             <li>Rice</li>
