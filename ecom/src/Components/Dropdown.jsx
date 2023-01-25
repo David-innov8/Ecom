9140dab7d81fb52514f80ab6fe8aacaf9e4ca674
David-innov8 <115226033+David-innov8@users.noreply.github.com>
2023-01-25 07:38:49 +0100

    Update Dropdown.jsx

--- ecom/src/Components/Dropdown.jsx
+++ ecom/src/Components/Dropdown.jsx
@@ -1,29 +1,50 @@
-import {React, usestate} from 'react'
+import {React, useState} from 'react'
 
  const Dropdown = () => {
 
-    const [isOpen, setIsOpen] = usestate(false)
+    const [isOpen, setIsOpen] = useState(false)
 
     const toggle = ()=>{
         setIsOpen(!isOpen)
     }
 
+    const [selectedOption, setSelectedOption] = useState(null)
+
+    const optionSelected=(target)=>()=>{
+        setSelectedOption(target)
+        setIsOpen(false)
+    }
+
+    const options = ['Mangoes','Rice','beans']
+     
   return (
     <div className=''>
-        <h1>Custom drop down</h1>
+   
         <div className= ' m-auto w-40 DropdownCotainer'>
-            <div onClick={toggle} className='DropDownHeader  mb-3 py-2 px-8 font-medium text-lg'>Mangoes</div>
-       
-            <div className="DropdownListContainer">
+            <div onClick={toggle} className='DropDownHeader  mb-3 py-2 px-8 font-medium cursor-pointer text-lg'>Mangoes</div>
+            <img></img>
+            {
+                isOpen && (
 
-
+                    <div className="DropdownListContainer">
+                        <ul className='DropdownList  p-0 m-0 pl-4 bg-white border-2 text-lg font-medium first:pt-3 ' >
+           {
+            options.map(options=>(
+                <li onClick={optionSelected(options)} key={Math.random()}>
+                        {options}
+                </li>
+            )    
+                
             
-        <ul className='DropdownList  p-0 m-0 pl-4 bg-white border-2 text-lg font-medium first:pt-3 ' >
-            <li>Mangoes</li>
-            <li>beans</li>
-            <li>Rice</li>
+            )
+           }
         </ul>
         </div>
+   
+                )
+
+
+            }
     </div>
     </div>
   )
